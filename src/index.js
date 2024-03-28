fd3909337674062e1371e88a31093dfbe5d76a63
nikhil rajput <nikhilkumar20192038@gmail.com>
2024-03-28 13:35:28 +0530

    Sdf

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <GoogleOAuthProvider clientId={process.env.YOUR_CLIENT_ID}>
+  <GoogleOAuthProvider clientId={`${process.env.YOUR_CLIENT_ID}`}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const proxy = {
-  target: process.env.YOUR_BACKEND_API,
+  target: process.env.YOUR_BACKEND_API || "http://localhost:4000",
   changeOrigin: true,
 };
 module.exports = function (app) {
